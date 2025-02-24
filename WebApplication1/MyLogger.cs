f61eed95da5cf52a0d5bbbf17834e306b8ca82f4
David Adamashvili <david.adamashvili71@gmail.com>
2025-02-24 17:52:48 +0400

    Commit project

---
+++ WebApplication1/MyLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1;
+
+public class MyLogger : IMyLogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+        // System.Diagnostics.Debug.WriteLine(message);
+    }
+}
